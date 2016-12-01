48058b4cd14d9eedc2bc2123f85c819563bc7f2f
Thushara Sampath Sathkumara <smtsampath@hotmail.com>
2016-11-30 21:50:57 -0500

    Update info.inc.php

--- info.inc.php
+++ info.inc.php
@@ -1,17 +1,17 @@
 	<?php
 
-	# Load Global debug mode
-	include("environment_variables.php");
+		# Load debug mode
+		include("environment_variables.php");
 
-	$host = "localhost";
-	$MySQLUsername = "root";
-	$MySQLPassword = "boise1";
-	$database = "Stunited";
+		$host = "localhost";
+		$MySQLUsername = "root";
+		$MySQLPassword = "boise1";
+		$database = "Stunited";
 
-	$SALT_A = "Salt";
-	$SALT_B = "Auth";
+		$SALT_A = "Salt";
+		$SALT_B = "Auth";
 
-	define('DB_HOST', $host);
-	define('DB_USER', $MySQLUsername);
-	define('DB_PASS', $MySQLPassword);
-	define('DB_NAME', $database);
+		define('DB_HOST', $host);
+		define('DB_USER', $MySQLUsername);
+		define('DB_PASS', $MySQLPassword);
+		define('DB_NAME', $database);
